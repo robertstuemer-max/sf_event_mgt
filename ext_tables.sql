a8fa48b2ce8badfe1a9ef9bc6dff455e53dbc808
robertstuemer-max <robert.stuemer@gmail.com>
2025-10-23 12:48:14 +0200

    Update ext_tables.sql Change type auf price to decimal

--- ext_tables.sql
+++ ext_tables.sql
@@ -12,7 +12,7 @@ CREATE TABLE tx_sfeventmgt_domain_model_event (
 	enddate int(11) DEFAULT '0' NOT NULL,
 	max_participants int(11) DEFAULT '0' NOT NULL,
 	max_registrations_per_user int(11) DEFAULT '1' NOT NULL,
-	price double(11,2) DEFAULT '0.00' NOT NULL,
+	price DECIMAL(11,2) DEFAULT 0.00 NOT NULL,
 	currency varchar(255) DEFAULT '' NOT NULL,
 	enable_payment tinyint(4) unsigned DEFAULT '0' NOT NULL,
 	restrict_payment_methods tinyint(4) unsigned DEFAULT '0' NOT NULL,
